f577f1477743a416c0f3e5425b4e41d4beaf99a2
Servicios Especiales <Servicios Especiales@SE08>
2019-05-31 21:41:03 -0500

    clase

---
+++ src/main/java/org/unitec/elementos1903/Cliente.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.unitec.elementos1903;
+
+import org.springframework.data.annotation.Id;
+
+/**
+ *
+ * @author Servicios Especiales
+ */
+public class Cliente {
+    
+    @Id
+    private Integer id;
+    private String nombre;
+    private String email;
+
+    public Cliente(Integer id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+    
+    public Cliente(){
+        
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
